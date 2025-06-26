8526e5aa379dda7805c03d984cacb1b8f48b6951
Jaronimo1337 <jaroslav.paskel@stud.techin.lt>
2025-06-26 13:46:06 +0300

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app">
+    <div class="h-100 text-center bg-green-500 rounded-xl p-5">
+      <h1 class="text-2xl font-bold text-gray-900">Weather App</h1>
+      <div><input type="text" placeholder="Enter a city name"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
